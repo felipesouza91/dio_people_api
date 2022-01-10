b58919c4984fe7ad32b162ab95580c2c211e354f
Felipe Souza <felipedb91@gmail.com>
2022-01-10 13:58:31 -0300

    feat: initial commit

---
+++ src/test/java/dev/fsantana/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fsantana.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
